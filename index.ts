144dd8642745200a4e766c62001f1cc52e9d4373
guicrispa <guicrispim1000@gmail.com>
2023-02-15 20:06:26 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -37,3 +37,31 @@ numbers.forEach((n) => {
 //tupla 
 let eu = ['dunga e Everson', 20];
 console.log(`meus brothers são ${eu[0]} e eles têm ${eu[1]} anos`);
+
+//enum (baseado em números, começando no zero, e para cada opção é assinalado um número incrementado por um. Isso é útil quando o valor em si não importa.)
+enum Color {
+  Red=1, 
+  Green=2, 
+  Blue=3,
+};
+let color: Color = Color.Green;
+
+//A sintaxe de tipo para declarar uma variável no TypeScript é incluir dois pontos (:) após o nome da variável, seguido por seu tipo.
+
+//any
+let semCerteza: any = 4;
+semCerteza = 'acho que sou um dunga';
+
+//functions
+function getSoma(n1: number, n2:number): number{
+  return n1 + n2;
+}
+
+console.log(`a soma de 5 e 8 é ${getSoma(5,8)}`)
+
+function getNomeCompleto(pessoa: {nome: string, sobrenome: string}): string{
+  return `${pessoa.nome} ${pessoa.sobrenome}`;
+}
+
+console.log(`meu nome completo é ${getNomeCompleto({nome: 'dunga', sobrenome: 'alves'})}`);
+
